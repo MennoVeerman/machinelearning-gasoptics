ab2739213a8eeeb17239d77a132d37c062583f63
Menno <menno.veerman@wur.nl>
2020-06-17 09:54:52 +0200

    first changes to extract_weights scripts

--- create_frozen_graph_extract_weights.py
+++ create_frozen_graph_extract_weights.py
@@ -3,34 +3,37 @@ import netCDF4 as nc
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 from tensorflow.python.framework import tensor_util
-from TrainNetwork2 import DNN_Regression
-basepath = "/scratch-shared/mveerman/Training/CABAUW/"
-datapath = "/scratch-shared/mveerman/PrepareData/CABAUW/"
-do_upper = False
-do_lower = True
-#NOTE: ONLY WORKS IF NETWORK IS TRAINED FOR BOTH ABOVE AND BELOW TROPOPAUSE
+from TrainNetwork import DNN_Regression
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--do_upper', default=True, type=bool)
+parser.add_argument('--do_lower', default=True, type=bool)
+parser.add_argument('--datapath', default='./', type=str)
+parser.add_argument('--trainpath', default='./', type=str)
+args = parser.parse_args()
 
 ### dictionary containing all means and st_devs
 def get_znorm(name):
-    Znorm={}
-    if do_upper:
-        Znorm["means_upper"] = np.loadtxt(datapath+"means0_%s.txt"%name,dtype=np.float32)
-        Znorm["stdev_upper"] = np.loadtxt(datapath+"stdev0_%s.txt"%name,dtype=np.float32)
-    if do_lower:
-        Znorm["means_lower"] = np.loadtxt(datapath+"means1_%s.txt"%name,dtype=np.float32)
-        Znorm["stdev_lower"] = np.loadtxt(datapath+"stdev1_%s.txt"%name,dtype=np.float32)
-    return Znorm
+    znorm={}
+    if args.do_upper:
+        znorm["means_upper"] = np.loadtxt(args.datapath+"means_upr_%s.txt"%name,dtype=np.float32)
+        znorm["stdev_upper"] = np.loadtxt(args.datapath+"stdev_upr_%s.txt"%name,dtype=np.float32)
+    if args.do_lower:
+        znorm["means_lower"] = np.loadtxt(args.datapath+"means_lwr_%s.txt"%name,dtype=np.float32)
+        znorm["stdev_lower"] = np.loadtxt(args.datapath+"stdev_lwr_%s.txt"%name,dtype=np.float32)
+    return znorm
 
-def get_model(modelpath,name,nlabel,nodes):
-    Znorm = get_znorm(name)
+def get_model(modelpath, name, n_label, nodes):
+    znorm = get_znorm(name)
     hyperparameters = {
-            'train'             : False,
-            'Nnodes'            : nodes,
-            'n_labels'          : nlabel,
-            'kernel_initializer': tf.glorot_uniform_initializer(),
-            'learning_rate'     : 0.01,
-            'Znorm'             : Znorm,
-            'keys'              : ["h2o","p_lay","t_lay"]+(name=="Planck")*["t_levB","t_levT"],
+            'train'              : False,
+            'n_nodes'            : nodes,
+            'n_labels'           : n_label,
+            'kernel_initializer' : tf.glorot_uniform_initializer(),
+            'learning_rate'      : 0.01,
+            'znorm'              : znorm,
+            'keys'               : ["h2o","p_lay","t_lay"]+(name=="Planck")*["t_levB","t_levT"],
             }
     model = tf.estimator.Estimator(
     model_fn = DNN_Regression,
@@ -42,82 +45,81 @@ def get_model(modelpath,name,nlabel,nodes):
 def get_placeholderdict(name):
     if name == "Planck":
         placeholderdict = {
-             "h2o": tf.placeholder(tf.float32, shape=[None], name="h2o"),
-             "p_lay": tf.placeholder(tf.float32, shape=[None], name="p_lay"),
-             "t_lay": tf.placeholder(tf.float32, shape=[None], name="t_lay"),
-             "tropo": tf.placeholder(tf.float32, shape=[None], name="tropo"),
-             "t_levB":tf.placeholder(tf.float32, shape=[None], name="t_levB"),
-             "t_levT":tf.placeholder(tf.float32, shape=[None], name="t_levT"),
+             "h2o"   : tf.placeholder(tf.float32, shape=[None], name="h2o"),
+             "p_lay" : tf.placeholder(tf.float32, shape=[None], name="p_lay"),
+             "t_lay" : tf.placeholder(tf.float32, shape=[None], name="t_lay"),
+             "tropo" : tf.placeholder(tf.float32, shape=[None], name="tropo"),
+             "t_levB": tf.placeholder(tf.float32, shape=[None], name="t_levB"),
+             "t_levT": tf.placeholder(tf.float32, shape=[None], name="t_levT"),
              }
     else:
         placeholderdict = {
-             "h2o": tf.placeholder(tf.float32, shape=[None], name="h2o"),
+             "h2o"  : tf.placeholder(tf.float32, shape=[None], name="h2o"),
              "p_lay": tf.placeholder(tf.float32, shape=[None], name="p_lay"),
              "t_lay": tf.placeholder(tf.float32, shape=[None], name="t_lay"),
              "tropo": tf.placeholder(tf.float32, shape=[None], name="tropo")}
     return placeholderdict
-### function to make frozen graph
-def frozen_graph_maker(export_dir,output_graph,outname):
-    with tf.Session(graph=tf.Graph()) as sess:
-        tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], export_dir)
-        #name of output node(s):
-        output_nodes = [outname]  
-        output_graph_def = tf.graph_util.convert_variables_to_constants(
-                sess, # The session is used to retrieve the weights
-                sess.graph_def,
-                output_nodes # The output node names are used to select the usefull nodes
-        ) 
-    # Finally we serialize and dump the output graph to the filesystem
-    with tf.gfile.GFile(output_graph, "wb") as f:
-            f.write(output_graph_def.SerializeToString())
-    with tf.Session() as sess:
-        with gfile.FastGFile(output_graph,"rb") as f:
-            graph_def = tf.GraphDef()
-            graph_def.ParseFromString(f.read())
-            sess.graph.as_default()
-            tf.import_graph_def(graph_def, name='')
-            const_nodes=[n for n in graph_def.node]# if n.op=='Const']
-        for n in range(len(const_nodes)):
-            print(n, const_nodes[n].name)    
 
-def extract_weights(graphpathL,graphpathU,ncgrp):
+### function to make frozen graph
+def frozen_graph_maker(export_dir, output_graph, output_node):
+    with tf.Session(graph=tf.Graph()) as session:
+        tf.saved_model.loader.load(session, [tf.saved_model.tag_constants.SERVING], export_dir)
+        output_graph_def = tf.graph_util.convert_variables_to_constants(
+                session,
+                session.graph_def,
+                [output_node]) 
+
+    with tf.gfile.GFile(output_graph, "wb") as gf:
+            gf.write(output_graph_def.SerializeToString())
+
+#    with tf.Session() as session:
+#        with gfile.FastGFile(output_graph,"rb") as gf:
+#            graph_def = tf.GraphDef()
+#            graph_def.ParseFromString(gf.read())
+#            session.graph.as_default()
+#            tf.import_graph_def(graph_def, name='')
+#            const_nodes=[n for n in graph_def.node]
+#        for n in range(len(const_nodes)):
+#            print(n, const_nodes[n].name)    
+
+def extract_weights(graphpath_lwr, graphpath_upr, ncgrp):
     graphs = []
-    if do_lower: 
-        graphs += [(graphpathL,'_lower')]
-    if do_upper: 
-        graphs += [(graphpathU,'_upper')]
+    if args.do_lower: 
+        graphs += [(graphpath_lwr,'_lower')]
+    if args.do_upper: 
+        graphs += [(graphpath_upr,'_upper')]
 
     for graph,uplow in graphs:
-        with tf.Session() as sess:
+        with tf.Session() as session:
             with gfile.FastGFile(graph,'rb') as f:
                 graph_def = tf.GraphDef()
                 graph_def.ParseFromString(f.read())
-                sess.graph.as_default()
+                session.graph.as_default()
                 tf.import_graph_def(graph_def, name='')
                 const_nodes=[n for n in graph_def.node if n.op=='Const']
         nodenames = [const_nodes[n].name for n in range(len(const_nodes))]
-        print("Name of all constant nodes:") #print all nodes that contain constant values (e.g. weights, biases)
+        print("names of all constant nodes:") 
         for n in range(len(const_nodes)):
             print(n, const_nodes[n].name)
 
-        Fmean = tensor_util.MakeNdarray(const_nodes[14].attr['value'].tensor)
-        Fstdv = tensor_util.MakeNdarray(const_nodes[15].attr['value'].tensor)
-        Lmean = tensor_util.MakeNdarray(const_nodes[16].attr['value'].tensor)
-        Lstdv = tensor_util.MakeNdarray(const_nodes[17].attr['value'].tensor)
-        Nin  = len(Fmean)
-        Nout = len(Lmean)        
+        ft_mean = tensor_util.MakeNdarray(const_nodes[14].attr['value'].tensor)
+        ft_stdv = tensor_util.MakeNdarray(const_nodes[15].attr['value'].tensor)
+        lb_mean = tensor_util.MakeNdarray(const_nodes[16].attr['value'].tensor)
+        lb_stdv = tensor_util.MakeNdarray(const_nodes[17].attr['value'].tensor)
+        n_in  = len(ft_mean)
+        n_out = len(lb_mean)        
         if uplow == "_lower":
-            ncgrp.createDimension("Nin",Nin)
-            ncgrp.createDimension("Nout",Nout)    
+            ncgrp.createDimension("Nin",n_in)
+            ncgrp.createDimension("Nout",n_out)    
             
         n1=ncgrp.createVariable("Fmean"+uplow,np.float32,("Nin",))
         n2=ncgrp.createVariable("Fstdv"+uplow,np.float32,("Nin",))
         n3=ncgrp.createVariable("Lmean"+uplow,np.float32,("Nout",))
         n4=ncgrp.createVariable("Lstdv"+uplow,np.float32,("Nout",))
-        n1[:] = Fmean 
-        n2[:] = Fstdv 
-        n3[:] = Lmean 
-        n4[:] = Lstdv 
+        n1[:] = ft_mean 
+        n2[:] = ft_stdv 
+        n3[:] = lb_mean 
+        n4[:] = lb_stdv 
         
         bias_idx = [nodenames.index(s) for s in nodenames if "bias" in s]
         for i in range(len(bias_idx)):
@@ -147,90 +149,73 @@ def extract_weights(graphpathL,graphpathU,ncgrp):
             print(n,"kernel" in n)
         print([nodenames.index(s) for s in nodenames if "bias" in s])
         print([nodenames.index(s) for s in nodenames if "kernel" in s])
-        if do_lower and not do_upper:
-            lowup1  = do_lower*'lower'+do_upper*'upper'
-            lowup2  = do_upper*'lower'+do_lower*'upper'
+        if args.do_lower and not args.do_upper:
+            lowup1  = args.do_lower*'lower'+args.do_upper*'upper'
+            lowup2  = args.do_upper*'lower'+args.do_lower*'upper'
             varnames = [name for name in ncgrp.variables if lowup1 in name]
             for varname in varnames:
                 oldvar = ncgrp.variables[varname]
                 newvar = ncgrp.createVariable(varname[:-5]+lowup2,np.float32,oldvar.dimensions)
                 newvar[:] = 0.
 
-def main_frozengraph(dirname,nodes,name,outname,nlabel):
-    serving_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(get_placeholderdict(name))    
-    modelpath = basepath+"%s/%s/"%(dirname,name) 
-#    if name =="Planck":
-#        modelpath = basepath+"Cabauw/%s/%s/"%(dirname,name) 
-    model = get_model(modelpath,name,nlabel,nodes)
-    savedmodel_path = model.export_savedmodel(modelpath, serving_fn,
-                        strip_default_attrs=True)
-    frozengraph_path = modelpath+"/"+"frozen_graph_%s.pb"%outname
-    
-    frozen_graph_maker(savedmodel_path,frozengraph_path,'output'+'_noexp'*(name=='SSA'))
-    tf.keras.backend.clear_session()
 def main_loop():
-    ###input function
-    for nodes,dirname in [([64],"1L-64/"),([64,64],"2L-64_64/"),([32],"1L-32/"),([32,32],"2L-32_32/"),([32,64,128],"3L-32_64_128/")]:
-        main_extractweights(dirname,nodes)
-        #for name,outname,nlabel in [("tauLW","tlw",256)]:#[("Planck","plk",768)]:#,("SSA","ssa",224),("tauLW","tlw",256),("tauSW","tsw",224)]:
-        #    main_frozengraph(dirname,nodes,name,outname,nlabel)
-
-def main_extractweights(dirname,nodes):
-    print(datapath+"weights_%s.nc"%dirname[3:])
-    ncfile = nc.Dataset(basepath+"weights_%s.nc"%dirname[3:-1],"w")    
+    for nodes,dirname in [([32],"1L-32/"), ([32,32],"2L-32_32/"), ([64],"1L-64/"), ([64,64],"2L-64_64/"), ([32,64,128],"3L-32_64_128/")]:
+        main_extractweights(dirname, nodes)
+        
+def main_extractweights(dirname, nodes):
+    ncfile = nc.Dataset(args.trainpath+"weights_%s.nc"%dirname[3:-1], "w")    
 
     grp = ncfile.createGroup("SSA"); name="SSA"
-    modelpath = basepath+"%s/%s/"%(dirname,name)  
+    modelpath = args.trainpath+"%s/%s/"%(dirname,name)  
     serving_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(get_placeholderdict(name))    
     model = get_model(modelpath,name,224,nodes)
     savedmodel_path = model.export_savedmodel(modelpath, serving_fn,
                             strip_default_attrs=True)
     frozengraph_pathL = modelpath+"/"+"frozen_graph_ssa_lower.pb"
     frozengraph_pathU = modelpath+"/"+"frozen_graph_ssa_upper.pb"
-    if do_lower: frozen_graph_maker(savedmodel_path,frozengraph_pathL,'output_lower')
-    if do_upper: frozen_graph_maker(savedmodel_path,frozengraph_pathU,'output_upper')
+    if args.do_lower: frozen_graph_maker(savedmodel_path,frozengraph_pathL,'output_lower')
+    if args.do_upper: frozen_graph_maker(savedmodel_path,frozengraph_pathU,'output_upper')
     extract_weights(frozengraph_pathL,frozengraph_pathU,grp)
     tf.keras.backend.clear_session()
     
     grp = ncfile.createGroup("TSW"); name="tauSW"
-    modelpath = basepath+"%s/%s/"%(dirname,name)  
+    modelpath = args.trainpath+"%s/%s/"%(dirname,name)  
     serving_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(get_placeholderdict(name))    
     model = get_model(modelpath,name,224,nodes)
     savedmodel_path = model.export_savedmodel(modelpath, serving_fn,
                             strip_default_attrs=True)
     frozengraph_pathL = modelpath+"/"+"frozen_graph_tsw_lower.pb"
     frozengraph_pathU = modelpath+"/"+"frozen_graph_tsw_upper.pb"
-    if do_lower: frozen_graph_maker(savedmodel_path,frozengraph_pathL,'output_lower')
-    if do_upper: frozen_graph_maker(savedmodel_path,frozengraph_pathU,'output_upper')
+    if args.do_lower: frozen_graph_maker(savedmodel_path,frozengraph_pathL,'output_lower')
+    if args.do_upper: frozen_graph_maker(savedmodel_path,frozengraph_pathU,'output_upper')
     extract_weights(frozengraph_pathL,frozengraph_pathU,grp)
     tf.keras.backend.clear_session()
     
     grp = ncfile.createGroup("TLW"); name="tauLW"
-    modelpath = basepath+"%s/%s/"%(dirname,name)  
+    modelpath = args.trainpath+"%s/%s/"%(dirname,name)  
     serving_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(get_placeholderdict(name))    
     model = get_model(modelpath,name,256,nodes)
     savedmodel_path = model.export_savedmodel(modelpath, serving_fn,
                             strip_default_attrs=True)
     frozengraph_pathL = modelpath+"/"+"frozen_graph_tlw_lower.pb"
     frozengraph_pathU = modelpath+"/"+"frozen_graph_tlw_upper.pb"
-    if do_lower: frozen_graph_maker(savedmodel_path,frozengraph_pathL,'output_lower')
-    if do_upper: frozen_graph_maker(savedmodel_path,frozengraph_pathU,'output_upper')
+    if args.do_lower: frozen_graph_maker(savedmodel_path,frozengraph_pathL,'output_lower')
+    if args.do_upper: frozen_graph_maker(savedmodel_path,frozengraph_pathU,'output_upper')
     extract_weights(frozengraph_pathL,frozengraph_pathU,grp)
     tf.keras.backend.clear_session()
     
     grp = ncfile.createGroup("Planck"); name="Planck"
-    modelpath = basepath+"%s/%s/"%(dirname,name)  
+    modelpath = args.trainpath+"%s/%s/"%(dirname,name)  
     serving_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(get_placeholderdict(name))    
     model = get_model(modelpath,name,768,nodes)
     savedmodel_path = model.export_savedmodel(modelpath, serving_fn,
                             strip_default_attrs=True)
     frozengraph_pathL = modelpath+"/"+"frozen_graph_plk_lower.pb"
     frozengraph_pathU = modelpath+"/"+"frozen_graph_plk_upper.pb"
-    if do_lower: frozen_graph_maker(savedmodel_path,frozengraph_pathL,'output_lower')
-    if do_upper: frozen_graph_maker(savedmodel_path,frozengraph_pathU,'output_upper')
+    if args.do_lower: frozen_graph_maker(savedmodel_path,frozengraph_pathL,'output_lower')
+    if args.do_upper: frozen_graph_maker(savedmodel_path,frozengraph_pathU,'output_upper')
     extract_weights(frozengraph_pathL,frozengraph_pathU,grp)
     ncfile.close()
 
 if __name__ == "__main__":
-#    main_extractweights("2L-64_64",[64,64])
     main_loop()
