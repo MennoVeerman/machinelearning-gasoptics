75b44b7669c4482f436571d059b9b3215254499d
MenoVeerman <menno.veerman@wur.nl>
2020-06-24 08:35:12 +0200

    delete individual input/output files at the end, saves some storages

--- rfmip/rfmip_main.py
+++ rfmip/rfmip_main.py
@@ -5,10 +5,10 @@ import subprocess
 import os
 
 nfile = 5
-rrtmgp_path =  "/path/to/rrtmgp-rte-cpp/folder"
-rrtmgp_build_dir = "rrtmgp/path/build/"
-rrtmgp_coeff_dir = "rrtmgp/path/rte-rrtmgp/rrtmgp/data/"
-rrtmgp_cloud_dir = "rrtmgp/path/rte-rrtmgp/extensions/cloud_optics/"
+rrtmgp_path =  "/path/to/rrtmgp-rte-cpp/folder/"
+rrtmgp_build_dir = rrtmgp_path+"build/"
+rrtmgp_coeff_dir = rrtmgp_path+"rte-rrtmgp/rrtmgp/data/"
+rrtmgp_cloud_dir = rrtmgp_path+"rte-rrtmgp/extensions/cloud_optics/"
 if not os.path.exists('test_rte_rrtmgp'): os.symlink(rrtmgp_build_dir+'test_rte_rrtmgp', 'test_rte_rrtmgp')
 if not os.path.exists('coefficients_lw.nc'): os.symlink(rrtmgp_coeff_dir+'rrtmgp-data-lw-g256-2018-12-04.nc', 'coefficients_lw.nc')
 if not os.path.exists('coefficients_sw.nc'): os.symlink(rrtmgp_coeff_dir+'rrtmgp-data-sw-g224-2018-12-04.nc', 'coefficients_sw.nc')
@@ -30,6 +30,12 @@ for ifile in range(nfile):
     subprocess.call(['./test_rte_rrtmgp', '--no-cloud-optics', '--no-fluxes', '--output-optical'])
     shutil.move('rte_rrtmgp_output.nc', 'data/rte_rrtmgp_output_{:03d}.nc'.format(ifile))
 
+#concetenate output and input file
 os.system('ncecat data/*input*  -u iter -O inputs.nc')
 os.system('ncecat data/*output* -u iter -O outputs.nc')
+
+#delete individual input/output files
+os.system('rm -f data/*input*')
+os.system('rm -f data/*output*')
+os.system('rm -r data')
 
